0904ddeeefa73ffe3273f5d13e68ebc6695e0732
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-10-17 00:30:37 +0530

    basic setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import "./App.css";
-import { Button } from "./components/ui/button";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import { ThemeProvider } from "./components/theme-provider";
+import { Spinner } from "@nextui-org/react";
+
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Products = lazy(() => import("./pages/Products"));
+const Transactions = lazy(() => import("./pages/Transactions"));
+const Customers = lazy(() => import("./pages/Customers"));
 
 function App() {
   return (
-    <>
-      <Button>Click Me</Button>
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Suspense fallback={<Spinner />}>
+          <Routes>
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/products" element={<Products />} />
+            <Route path="/admin/customers" element={<Customers />} />
+            <Route path="/admin/transactions" element={<Transactions />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
